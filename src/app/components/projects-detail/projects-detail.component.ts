3a6f24a0b6f1d7e76970d07ec3fc41c222919232
Jose Ledezma <jose.ledezma@sistemasgalileo.com>
2020-03-09 19:20:56 -0600

    Primer commit

---
+++ src/app/components/projects-detail/projects-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-detail',
+  templateUrl: './projects-detail.component.html',
+  styles: []
+})
+export class ProjectsDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/reel/reel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reel',
+  templateUrl: './reel.component.html',
+  styles: []
+})
+export class ReelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
