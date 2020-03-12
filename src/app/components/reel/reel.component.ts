c23f1e3143206415c3f83fe92235549ad86e58d8
Jose Ledezma <jose.ledezma@sistemasgalileo.com>
2020-03-12 16:50:38 -0600

    Se agrega el reel

--- src/app/components/reel/reel.component.ts
+++ src/app/components/reel/reel.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import Player from '@vimeo/player'
+
+import { environment } from "../../../environments/environment";
 
 @Component({
   selector: 'app-reel',
   templateUrl: './reel.component.html',
   styles: []
 })
-export class ReelComponent implements OnInit {
+export class ReelComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('player_container', { static: false }) playerContainer;
+  private player: Player;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // child is set
+    this.player = new Player(this.playerContainer.nativeElement, {
+      id: environment.reelVideoUrl,
+      muted: false,
+      quality: '1080p',
+      controls: false,
+      color: '#000E18'
+    });
+    console.log(Player);
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  reelVideoUrl: 349083552
 };
